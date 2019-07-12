60d8c46711e385b9f966b7f0c09a1e3520ba8fcb
contact.iscsr@gmail.com <mihirt@digitalinfuzion.com>
2019-07-12 13:24:45 +0530

    Added Naive Bayes

---
+++ Naive Bayes/naive-bayes.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import matplotlib.pyplot as plt  
+from sklearn.naive_bayes import GaussianNB
+import numpy as np
+from sklearn.metrics import mean_squared_error   
+
+'''
+We can use Scikit learn to build a simple linear regression model
+you can use it use it like 
+model = GaussianNB()
+
+'''
+
+
+#### Your Code Goes Here #### 
+
+## Step 1: Load Data from CSV File ####
+
+
+## Step 2: Plot the Data ####
+
+
+## Step 3: Build a NB Model ####
+
+
+## Step 4: Print Predicted vs Actuals ####
+
+
+## Step 5: Estimate Error #### 
